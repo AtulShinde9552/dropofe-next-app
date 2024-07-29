b3d686780fea6e94ed16270ad274eb2ac8e9b4cb
AtulShinde9552 <atul387659@gmail.com>
2024-07-29 20:22:33 +0530

    dynamic value add

--- app/api/reply/route.ts
+++ app/api/reply/route.ts
@@ -59,5 +59,5 @@ export async function PUT(request: Request) {
 }
 
 // Exporting configuration for Vercel
-export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const maxDuration = 30; // This function can run for a maximum of 5 seconds
 export const dynamic = 'force-dynamic';
